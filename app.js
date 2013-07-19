9b9af0766d0fa4c2af6a724e58c804e80a4c82fb
Igor Bronowicki <ibronovytskyi@ebay.com>
2013-07-19 20:19:56 +0300

    deleted some stuff

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/*
- * This is server-side JavaScript, intended to be run with NodeJS.
- * It runs a WebSocket server on top of an HTTP server, using an external
- * websocket library from https://github.com/miksago/node-websocket-server/
- * If it gets an HTTP request for "/" it returns the chat client HTML file.
- * Any other HTTP requests return 404. Messages received via the
- * WebSocket protocol are simply broadcast to all active connections.
- */
 var app = require('http').createServer(handler)
     , io = require('socket.io').listen(app)
     , fs = require('fs');
